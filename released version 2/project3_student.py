b7a02b1a42fac2d5e5f6991e44cd643a0ec52738
Hikari Senju <hsenju@gmail.com>
2015-05-02 17:47:48 -0400

    first part done

---
+++ released version 2/project3_student.py
@@ -0,0 +1,240 @@
+#this file contains major functions
+import random as ra
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as pat
+from numpy import linalg as LA
+
+# Reads a data matrix from file.
+# Output: X: data matrix.
+def readData(file):
+    X = []
+    with open(file,"r") as f:
+        for line in f:
+            X.append(map(float,line.split(" ")))
+    return np.array(X)
+    
+# plot 2D toy data
+# input: X: n*d data matrix;
+#        K: number of mixtures;
+#        Mu: K*d matrix, each row corresponds to a mixture mean;
+#        P: K*1 matrix, each entry corresponds to the weight for a mixture;
+#        Var: K*1 matrix, each entry corresponds to the variance for a mixture;
+#        Label: n*K matrix, each row corresponds to the soft counts for all mixtures for an example
+#        title: a string represents the title for the plot
+def plot2D(X,K,Mu,P,Var,Label,title):
+    r=0.25
+    color=["r","b","k","y","m","c"]
+    n,d = np.shape(X)
+    per= Label/(1.0*np.tile(np.reshape(np.sum(Label,axis=1),(n,1)),(1,K)))
+    fig=plt.figure()
+    plt.title(title)
+    ax=plt.gcf().gca()
+    ax.set_xlim((-20,20))
+    ax.set_ylim((-20,20))
+    for i in xrange(len(X)):
+        angle=0
+        for j in xrange(K):
+            cir=pat.Arc((X[i,0],X[i,1]),r,r,0,angle,angle+per[i,j]*360,edgecolor=color[j])
+            ax.add_patch(cir)
+            angle+=per[i,j]*360
+    for j in xrange(K):
+        sigma = np.sqrt(Var[j])
+        circle=plt.Circle((Mu[j,0],Mu[j,1]),sigma,color=color[j],fill=False)
+        ax.add_artist(circle)
+        text=plt.text(Mu[j,0],Mu[j,1],"mu=("+str("%.2f" %Mu[j,0])+","+str("%.2f" %Mu[j,1])+"),stdv="+str("%.2f" % np.sqrt(Var[j])))
+        ax.add_artist(text)
+    plt.axis('equal')
+    plt.show()
+
+# initialization for k means model for toy data
+# input: X: n*d data matrix;
+#        K: number of mixtures;
+#        fixedmeans: is an optional variable which is
+#        used to control whether Mu is generated from a deterministic way
+#        or randomized way
+# output: Mu: K*d matrix, each row corresponds to a mixture mean;
+#         P: K*1 matrix, each entry corresponds to the weight for a mixture;
+#         Var: K*1 matrix, each entry corresponds to the variance for a mixture;    
+def init(X,K,fixedmeans=False):
+    n, d = np.shape(X)
+    P=np.ones((K,1))/float(K)
+
+    if (fixedmeans):
+        assert(d==2 and K==3)
+        Mu = np.array([[4.33,-2.106],[3.75,2.651],[-1.765,2.648]])
+    else:
+        # select K random points as initial means
+        rnd = np.random.rand(n,1)
+        ind = sorted(range(n),key = lambda i: rnd[i])
+        Mu = np.zeros((K,d))
+        for i in range(K):
+            Mu[i,:] = np.copy(X[ind[i],:])
+
+    Var=np.mean( (X-np.tile(np.mean(X,axis=0),(n,1)))**2 )*np.ones((K,1))
+    return (Mu,P,Var)
+
+# K Means method
+# input: X: n*d data matrix;
+#        K: number of mixtures;
+#        Mu: K*d matrix, each row corresponds to a mixture mean;
+#        P: K*1 matrix, each entry corresponds to the weight for a mixture;
+#        Var: K*1 matrix, each entry corresponds to the variance for a mixture;
+# output: Mu: K*d matrix, each row corresponds to a mixture mean;
+#         P: K*1 matrix, each entry corresponds to the weight for a mixture;
+#         Var: K*1 matrix, each entry corresponds to the variance for a mixture;
+#         post: n*K matrix, each row corresponds to the soft counts for all mixtures for an example
+def kMeans(X, K, Mu, P, Var):
+    prevCost=-1.0; curCost=0.0
+    n=len(X)
+    d=len(X[0])
+    while abs(prevCost-curCost)>1e-4:
+        post=np.zeros((n,K))
+        prevCost=curCost
+        #E step
+        for i in xrange(n):
+            post[i,np.argmin(np.sum(np.square(np.tile(X[i,],(K,1))-Mu),axis=1))]=1
+        #M step
+        n_hat=np.sum(post,axis=0)
+        P=n_hat/float(n)
+        curCost = 0
+        for i in xrange(K):
+            Mu[i,:]= np.dot(post[:,i],X)/float(n_hat[i])
+            # summed squared distance of points in the cluster from the mean
+            sse = np.dot(post[:,i],np.sum((X-np.tile(Mu[i,:],(n,1)))**2,axis=1))
+            curCost += sse
+            Var[i]=sse/float(d*n_hat[i])
+        print curCost
+    # return a mixture model retrofitted from the K-means solution
+    return (Mu,P,Var,post) 
+# RMSE criteria
+# input: X: n*d data matrix;
+#        Y: n*d data matrix;
+# output: RMSE
+def rmse(X,Y):
+    return np.sqrt(np.mean((X-Y)**2))
+
+def N(X,Mu,Var,d):
+    return (1/(np.power((2*np.pi*Var[0]),(d/2))))*np.exp((-1/(2*Var[0]))*np.power(np.linalg.norm(X-Mu),2))
+
+def summation(X,K,Mu,P,Var,d):
+    s = 0
+    for l in range(K):
+        s += P[l]*N(X,Mu[l],Var[l],d)
+    return s
+
+# E step of EM algorithm
+# input: X: n*d data matrix;
+#        K: number of mixtures;
+#        Mu: K*d matrix, each row corresponds to a mixture mean;
+#        P: K*1 matrix, each entry corresponds to the weight for a mixture;
+#        Var: K*1 matrix, each entry corresponds to the variance for a mixture;
+# output:post: n*K matrix, each row corresponds to the soft counts for all mixtures for an example
+#        LL: a Loglikelihood value
+def Estep(X,K,Mu,P,Var):
+    n,d = np.shape(X) # n data points of dimension d
+    post = np.zeros((n,K)) # posterior probabilities to tbd
+    LL = 0
+    for i in range (n):
+        s = summation(X[i],K,Mu,P,Var,d)
+        innersum = 0
+        for j in range (K):
+            post[i,j] = P[j]*N(X[i],Mu[j],Var[j],d)/s;
+            innersum += P[j]*N(X[i],Mu[j],Var[j],d)
+        LL += np.log(innersum)
+    return (post,LL)
+
+# M step of EM algorithm
+# input: X: n*d data matrix;
+#        K: number of mixtures;
+#        Mu: K*d matrix, each row corresponds to a mixture mean;
+#        P: K*1 matrix, each entry corresponds to the weight for a mixture;
+#        Var: K*1 matrix, each entry corresponds to the variance for a mixture;
+#        post: n*K matrix, each row corresponds to the soft counts for all mixtures for an example
+# output:Mu: updated Mu, K*d matrix, each row corresponds to a mixture mean;
+#        P: updated P, K*1 matrix, each entry corresponds to the weight for a mixture;
+#        Var: updated Var, K*1 matrix, each entry corresponds to the variance for a mixture;
+
+def Mstep(X,K,Mu,P,Var,post):
+    n,d = np.shape(X) # n data points of dimension d
+    for j in range (K):
+        nsum = 0
+        usum = 0
+        vsum = 0
+        for t in range (n):
+            nsum += post[t,j]
+            usum += post[t,j] * X[t]
+            vsum += post[t,j] * np.power(np.linalg.norm(X[t]-Mu[j]),2)
+        P[j] = nsum/n
+        Mu[j] = usum/nsum
+        Var[j] = vsum/(nsum*d)
+    print "P: " + str(P)
+    print "Mu: " + str(Mu)
+    print "Var: " + str(Var)
+    return (Mu,P,Var)
+
+def EMAlgorithm(X, K, Mu, P, Var):
+    prevCost=-1.0; curCost=0.0
+    n=len(X)
+    d=len(X[0])
+    while abs(prevCost-curCost)>1e-4:
+        post=np.zeros((n,K))
+        prevCost=curCost
+        (post,LL) = Estep(X,K,Mu,P,Var)
+        (Mu,P,Var) = Mstep(X,K,Mu,P,Var,post)
+        curCost = LL
+        print "cost: " + str(curCost)
+    # return a mixture model retrofitted from the K-means solution
+    return (Mu,P,Var,post) 
+
+# ----------------------------------------------------------------------------------------------------
+# mixture of Guassians
+# input: X: n*d data matrix;
+#        K: number of mixtures;
+#        Mu: K*d matrix, each row corresponds to a mixture mean;
+#        P: K*1 matrix, each entry corresponds to the weight for a mixture;
+#        Var: K*1 matrix, each entry corresponds to the variance for a mixture;
+# output: Mu: updated Mu, K*d matrix, each row corresponds to a mixture mean;
+#         P: updated P, K*1 matrix, each entry corresponds to the weight for a mixture;
+#         Var: updated Var, K*1 matrix, each entry corresponds to the variance for a mixture;
+#         post: updated post, n*K matrix, each row corresponds to the soft counts for all mixtures for an example
+#         LL: Numpy array for Loglikelihood values
+
+def mixGauss(X,K,Mu,P,Var):
+    n,d = np.shape(X) # n data points of dimension d
+    post = np.zeros((n,K)) # posterior probs tbd
+    
+    #Write your code here
+    #Use function Estep and Mstep as two subroutines
+    
+    return (Mu,P,Var,post,LL)
+# fill incomplete Matrix
+# input: X: n*d incomplete data matrix;
+#        K: number of mixtures;
+#        Mu: K*d matrix, each row corresponds to a mixture mean;
+#        P: K*1 matrix, each entry corresponds to the weight for a mixture;
+#        Var: K*1 matrix, each entry corresponds to the variance for a mixture;
+# output: Xnew: n*d data matrix with unrevealed entries filled
+def fillMatrix(X,K,Mu,P,Var):
+    n,d = np.shape(X)
+    Xnew = np.copy(X)
+
+    #Write your code here       
+    
+    return Xnew
+
+# Bayesian Information Criterion (BIC) for selecting the number of mixture components
+# input:  n*d data matrix X, a list of K's to try 
+# output: the highest scoring choice of K
+def BICmix(X,Kset):
+    n,d = np.shape(X)
+
+    #Write your code here
+    
+    return K
+
+K=3
+X = readData("toy_data.txt")
+(Mu,P,Var) = init(X,K,fixedmeans=True)
+(Mu,P,Var,post)  = EMAlgorithm(X, K, Mu, P, Var) 
+plot2D(X,K,Mu,P,Var,post,"k-means k="+str(K))
